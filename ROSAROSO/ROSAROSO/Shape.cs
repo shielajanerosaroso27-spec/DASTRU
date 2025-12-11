e2b5dcbb8497f5976839a2b2d5d3326d04c4c0b4
ACT-STUDENT <ACT-STUDENT@LAB4-PC33>
2025-12-11 17:21:01 +0800

    not final

---
+++ ROSAROSO/ROSAROSO/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROSAROSO
+{
+    public class Shape
+    {
+        //Base class
+        public string Color {  get; set; }
+
+        public Shape (string color)
+        {
+            Color = color;  
+        }
+
+        //Virtual method for Shape
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+
+        //Derived class
+
+        
+    }
+    public class Circle : Shape
+    {
+        //Base class
+        public double Radius { get; set; }
+
+        public Circle(string color, double radius) : base(color)
+        {
+            Radius = radius;
+        }
+
+        //Virtual method for Shape
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+    }
+    
+}
