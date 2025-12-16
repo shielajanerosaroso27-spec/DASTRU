e7b5ab3ad3c9246e67f3a10e8e02480271346933
shielajanerosaroso27-spec <shielajanerosaroso27@gmail.com>
2025-12-16 14:05:49 +0800

    Add files via upload

---
+++ ACTIVITY 1/Single Dimensional Array in Procedural Programming/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Dimensional_Array_in_Procedural_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = new string[5];
+
+            Console.WriteLine("Enter at least 5  Name of Cities in Cebu.\n");
+
+            // INPUT USING FOR LOOP
+            for (int x = 0; x < cities.Length; x++)
+            {
+                Console.Write("Enter city name {0} : " , x +1);
+                cities[x] = Console.ReadLine(); 
+            }
+
+            Array.Sort(cities);
+
+            Console.WriteLine("\n === The cities in alphabetical order are: ===");
+
+            foreach (string city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadKey();
+            
+        }
+        
+    }
+}
---
+++ ACTIVITY 1/Single Dimensional Array in Procedural Programming/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Single Dimensional Array in Procedural Programming")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Single Dimensional Array in Procedural Programming")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c18269b-78ae-472c-839d-22f9381dd869")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
